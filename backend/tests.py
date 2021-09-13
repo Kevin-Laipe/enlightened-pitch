53d5f5b6fcee5e4695e1e7cf27931f6942ec476b
Kevin Laipe <kevinlaipe@hotmail.ch>
2021-09-13 15:58:15 +0200

    Add fake test

--- backend/tests.py
+++ backend/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+import pytest
+from django.contrib.auth.models import User
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_user_create():
+  User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+  assert User.objects.count() == 1
