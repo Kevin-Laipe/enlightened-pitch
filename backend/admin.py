644e32f07f7f0ec4d752bec8757fbd881c40e9ac
Kevin Laipe <kevinlaipe@hotmail.ch>
2021-09-17 13:48:09 +0200

    Add forms for User

--- backend/admin.py
+++ backend/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import Card, Printing, Set, Keyword, Subtype, Talent, Releasenote, Stat, CardKeyword, CardSubtype, CardTalent, CardReleasenote, CardStat, User, Copy, Deck, DeckCard
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
 
 admin.site.register(Card)
 admin.site.register(Printing)
@@ -15,7 +36,7 @@ admin.site.register(CardSubtype)
 admin.site.register(CardTalent)
 admin.site.register(CardReleasenote)
 admin.site.register(CardStat)
-admin.site.register(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Copy)
 admin.site.register(Deck)
 admin.site.register(DeckCard)
