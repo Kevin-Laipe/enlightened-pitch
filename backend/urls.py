4dc03c53e027cddf653e20d7000f4592cdc2dc67
Kevin Laipe <kevinlaipe@hotmail.ch>
2021-09-29 10:29:27 +0200

    Add Swagger/Redoc

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,7 @@
-from django.urls.conf import include
-from rest_framework import routers
+from rest_framework import routers, permissions
 from django.urls import include, path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from backend import views
 
@@ -16,6 +17,21 @@ router.register(r'subtypes', views.SubtypeViewSet)
 router.register(r'stats', views.StatViewSet)
 router.register(r'cardstats', views.CardStatViewSet)
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Enlightened Pitch API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ]
