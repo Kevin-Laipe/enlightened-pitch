5ee0ea202753e6ebb41589abf0926ece030c6f41
Kevin Laipe <kevinlaipe@hotmail.ch>
2021-10-04 17:24:34 +0200

    Starting React frontend

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+            enlightenedpich.com is in no way affiliated with Legend Story Studios. Flesh and Blood™, and set names are trademarks of Legend Story Studios®. Flesh and Blood characters, cards, logos, and art are property of Legend Story Studios.
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
